b177802871a2f2f3dc7f49409d4586f46aac8c25
Muhammad Bilal Kadri <bilal.kadri@gmail.com>
2025-04-06 17:52:07 +0300

    Jetson Xavier NX was upgraded  to Ubuntu 20 along with JetPack 5. This new setup provided better compatibility with the required Python and CUDA libraries.

--- plotting_results_offline.py
+++ plotting_results_offline.py
@@ -60,44 +60,24 @@ plt.savefig('Figures_for_the_paper/pf_RMSE_Longitude_GPU.png', dpi=400)
 plt.show()
 
 
-# FIGURE 15
-# PF Result
-# Bar Chart
-x = [1, 2, 3, 4,5,6,7,8,9,10]
-y1 =[0.000215,0.000142,0.000144,0.000132,0.000147,0.000155,0.000142,0.000132,0.000127,0.000142]
-y2 = [0.000054,0.0000543,0.000055,0.0000546,0.0000544,0.0000546,0.0000542,0.0000546,0.0000541,0.000054]
-y3 = [0.017972946166992188, 0.00048041343688964844, 0.0004963874816894531, 0.00046253204345703125, 0.00046634674072265625, 0.0004475116729736328, 0.0005228519439697266, 0.0004639625549316406, 0.0004470348358154297, 0.0004947185516357422]
-plt.figure(figsize=(900/100, 900/100), dpi=100)  # 900x900 pixels
-# plt.title('Figure 15')
-plt.title("CPU and GPU Time Comparison for PF prediction")
-plt.plot(x, y1,label="cpu time", color='blue',marker="s")
-plt.plot(x, y2,label="gpu time", color='red',marker="s")
-plt.plot(x, y3,label="gpu time NVIDIA Jetson Xavier", color='green',marker="s")
+## Plot FIGURE 15
 
-plt.xlabel('Iteration Number')
-plt.ylabel('Execution Time per Data Point ESKF')
-plt.legend()
-plt.grid(True, linestyle='--', alpha=0.7)  # Add grid lines (dashed, slightly transparent)
-#plt.ylim([0, 0.0003])
-plt.savefig('Figures_for_the_paper/pf_time_cuda_GPU.png', dpi=400)
-plt.show()
-
-## Another Plot FIGURE 15
-
-# Bar Chart
+# Line Plot
 x = [1, 2, 3, 4,5,6,7,8,9]
 y1 =[0.000142,0.000144,0.000132,0.000147,0.000155,0.000142,0.000132,0.000127,0.000142]
 y2 = [0.0000543,0.000055,0.0000546,0.0000544,0.0000546,0.0000542,0.0000546,0.0000541,0.000054]
-y3 = [0.00048041343688964844, 0.0004963874816894531, 0.00046253204345703125, 0.00046634674072265625, 0.0004475116729736328, 0.0005228519439697266, 0.0004639625549316406, 0.0004470348358154297, 0.0004947185516357422]
+y3 = [0.0004116535186767578,0.00037508010864257812,0.0003592967987060547,0.0003504037857055664,0.0003559589385986328, 0.00035331249237060547,0.00033483505249023438,0.0003439188003540039,0.0003335237503051758]
+
+
 plt.figure(figsize=(900/100, 900/100), dpi=100)  # 900x900 pixels
 # plt.title('Figure 15')
-plt.title("CPU and GPU Time Comparison for PF prediction")
+plt.title("Computation time Comparison for PF prediction")
 plt.plot(x, y1,label="CPU time", color='blue',marker="s")
 plt.plot(x, y2,label="GPU time", color='red',marker="s")
 plt.plot(x, y3,label="NVIDIA Jetson Xavier", color='green',marker="s")
 
 plt.xlabel('Iteration Number')
-plt.ylabel('Execution Time per Data Point ESKF')
+plt.ylabel('Execution Time per Data Point PF')
 plt.legend()
 plt.grid(True, linestyle='--', alpha=0.7)  # Add grid lines (dashed, slightly transparent)
 #plt.ylim([0, 0.0003])
